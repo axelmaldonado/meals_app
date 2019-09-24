7df038de1daec0556bd6e2226c8af8d3c4daaf76
Axel Maldonado-Hernandez <devaxel@smallbusinessbank.com>
2019-09-24 13:39:55 -0500

    implement home

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -1,16 +1,25 @@
 import 'package:flutter/material.dart';
 
+import './dummy-data.dart';
+import './category.item.dart';
+
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      children: <Widget>[],
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 1.5,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
-      ),
-    );
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('iMeals'),
+        ),
+        body: GridView(
+          children: DUMMY_CATEGORIES
+              .map((catData) => CategoryItem(catData.title, catData.color))
+              .toList(),
+          gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 200,
+            childAspectRatio: 1.5,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20,
+          ),
+        ));
   }
 }
